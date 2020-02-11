a651ec05a33673897374e1dc70aac7dc206a8fd9
Michael Beale <michael@bealefamily.net>
2020-02-11 07:36:55 -0800

    trying to make namespaces work

--- ns.ts
+++ ns.ts
@@ -1,17 +1,8 @@
 /**
  * Automation, process control and robotic controllers
  */
-//% groups="['PID', `Filters', 'Behaviors', 'Move']"
+//% groups="['PID', `Filters', 'Behaviors']"
 //% color=#909090 weight=80 icon="\uf277"
 namespace automation {
-
-}
-
-/**
- * MoveDifferential, process control and robotic controllers
- */
-//% groups="['Arc']"
-//% color=#090909 weight=90 icon="\uf277"
-namespace Move {
 
 }
